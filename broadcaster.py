9214bcef37ce95fdcc538b7aae95d6c069f93434
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-04-02 21:56:46 +0200

    Relocate function

--- broadcaster.py
+++ broadcaster.py
@@ -8,7 +8,6 @@
 ##################################################################
 
 
-from typing import Optional
 import logging
 import asyncio
 
@@ -24,30 +23,11 @@ log = logging.getLogger(__name__)
 
 
 class Broadcaster:
-    def __init__(self) -> None:
-        self.channel = config.twitch.channel.lower()
-        self.ready = False
+    def __init__(self):
         self.session = requests.Session()
-        log.info(f"Loaded broadcaster {self.channel}")
-
-    def get_infos(self, token) -> None:
-        if not self.ready:
-            log.debug(f"Get infos for {self.channel}...")
-            r = self.session.get(
-                f"https://api.twitch.tv/helix/users?login={self.channel}",
-                headers={
-                    "User-Agent": USER_AGENT,
-                    "Authorization": token,
-                    "Client-Id": config.twitch.client_id
-                }
-            )
-            log.debug(f"HTTP response: {r.text.strip()}")
-            j = raise_on_error(r)
-            self.id = j['data'][0]['id']
-            self.display_name = j['data'][0]['display_name']
-            self.image = j['data'][0]['profile_image_url']
-            log.info(f"Loaded infos for {self.display_name} ({self.id})")
-            self.ready = True
+        self.session.headers = {"User-Agent": USER_AGENT}
+        self.ready = False
+        log.info(f"Loaded broadcaster {config.twitch.channel.lower()}")
 
     async def get_acress(self):
         while True:
@@ -58,9 +38,8 @@ class Broadcaster:
                 mods = c.fetchall()
             for mod in mods:
                 r = self.session.get(
-                    "https://api.twitch.tv/helix/users",
+                    "https://api.twitch.tv/helix/users",  # TODO: in constants.py
                     headers={
-                        "User-Agent": USER_AGENT,
                         "Authorization": f"Bearer {mod['token']}",
                         "Client-Id": config.twitch.client_id
                     }
@@ -68,7 +47,7 @@ class Broadcaster:
                 log.debug(f"HTTP response: {r.text.strip()}")
                 j = raise_on_error(r, "Invalid OAuth token")
                 if "message" in j:
-                    log.debug("Token expired. Delete...")
+                    log.info(f"Token from {mod['id']} expired")
                     with Cursor() as c:
                         c.execute(
                             "DELETE FROM mods WHERE id = %s",
@@ -78,7 +57,22 @@ class Broadcaster:
                         )
                     continue
                 if not self.ready:
-                    self.get_infos(f"Bearer {mod['token']}")
+                    # get infos about the broadcaster
+                    log.debug(f"Get infos for {config.twitch.channel.lower()}...")
+                    r = self.session.get(
+                        f"https://api.twitch.tv/helix/users?login={config.twitch.channel.lower()}",  # TODO: in constants.py
+                        headers={
+                            "Authorization": f"Bearer {mod['token']}",
+                            "Client-Id": config.twitch.client_id
+                        }
+                    )
+                    log.debug(f"HTTP response: {r.text.strip()}")
+                    j = raise_on_error(r)
+                    self.id = j['data'][0]['id']
+                    self.display_name = j['data'][0]['display_name']
+                    self.image = j['data'][0]['profile_image_url']
+                    log.info(f"Loaded infos for {self.display_name} ({self.id})")
+                    self.ready = True
                 return mod['token'], j['data'][0]['id'], self.id
-            log.warning(f"No valid tokens found for {'channek_id'}. Try again soon...")
-            await asyncio.sleep(60)
+            log.warning(f"No valid tokens found for {config.twitch.channel}. Try again soon...")
+            await asyncio.sleep(60)  # TODO: configurable
